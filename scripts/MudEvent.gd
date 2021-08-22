97413515163433c7c11bb078fb1605e453df3951
NewPirateOfUASeas <75931217+NewPirateOfUASeas@users.noreply.github.com>
2021-08-22 15:36:51 +0100

    edited

--- scripts/MudEvent.gd
+++ scripts/MudEvent.gd
@@ -5,7 +5,7 @@ onready var player = get_tree().get_nodes_in_group('Player')[0]
 
 func _ready():
 	choice_area.question_panel.first.text = 'Обойти'
-	choice_area.question_panel.second.text = 'Пойти'
+	choice_area.question_panel.second.text = 'Войти'
 
 func _on_ChoiceArea2_first():
 	player.translation.z -= 0.5
